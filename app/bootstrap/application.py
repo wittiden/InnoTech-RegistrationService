95a1e4dd5d5445c4bd2335034874e3df41f35a1e
Denis <ar.denis.by@gmail.com>
2026-09-20 22:07:05 +0300

    hotfix(container): integrate dishka and FastAPI

--- app/bootstrap/application.py
+++ app/bootstrap/application.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from dishka.integrations.fastapi import setup_dishka
 
 from app.common.config import application_config
+from app.container.container import async_container
 
 
 def setup_application() -> FastAPI:
@@ -12,5 +14,7 @@ def setup_application() -> FastAPI:
                     ' проверка email на валидность и отправляем на него ссылку,'
                     ' при переходе по ссылке - пользователь авторизуется в системе'
     )
+
+    setup_dishka(async_container, app)
 
     return app
