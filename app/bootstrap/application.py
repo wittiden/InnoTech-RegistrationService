69652e3dcb1030bfb4d494c823e29c633cbad317
Denis <ar.denis.by@gmail.com>
2026-09-21 13:38:29 +0300

    chore(app): add async lifespan

--- app/bootstrap/application.py
+++ app/bootstrap/application.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from app.common.config import application_config
+from app.infrastructure.http.lifespan import lifespan
 
 
 def setup_application() -> FastAPI:
     app = FastAPI(
+        lifespan=lifespan,
         title='RegistrationService',
         version=application_config.PROJECT_VERSION,
         summary='RegistrationService API',
